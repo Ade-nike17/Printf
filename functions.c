35000b79473cc32d7fa9c04f0cc2744af91b13b5
Ade-nike17 <perfectnyke@gmail.com>
2023-06-25 13:02:55 -0700

    conv specifier c, s, %

---
+++ functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * print_string - writes a null terminated string to the output stream
+ * @args: list of arguments
+ *
+ * Return: mumber of characters printed
+ */
+
+int print_string(va_list args)
+{
+	char *str;
+	int i;
+
+	str = va_arg(args, char *);
+
+	if (str == NULL)
+	str = "(null)";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (i);
+}
+
+
+/**
+ * print_char - function prints a single character
+ *
+ * @args: list of arguments
+ * Return: number of characters printed
+ */
+
+/* writes a single char to the output stream */
+int print_char(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+
+	return (1);
+}
+
+
+/**
+ * print_percent - prints only percentage
+ *
+ * @args: list of arguments
+ *
+ * Return: character printed
+ */
+
+int print_percent(__attribute__((unused)) va_list args)
+{
+	return (_putchar('%'));
+}
