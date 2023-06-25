60afbe6585eccf86894e581421d4e9117377aa77
Afeh <balogunafebu22@gmail.com>
2023-06-25 20:33:37 +0100

    main.h with function prototypes

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* function prototypes */
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(va_list args);
+int print_char(va_list args);
+
+
+/**
+ * struct printer - struct for printer functions
+ *
+ * @type: the conversion specifier
+ * @print: the function to print the argument
+ */
+
+typedef struct printer
+{
+	char *type;
+	int (*print)(va_list);
+} printer_t;
+
+int format_printer(const char *format, va_list args, printer_t printers[]);
+
+#endif /* MAIN_H */
