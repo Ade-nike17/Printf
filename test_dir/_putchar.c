cee74aa290e778caf3a26416024f0941ef0c90c2
Afeh <balogunafebu22@gmail.com>
2023-06-25 20:59:35 +0100

    Added files for the test directory

---
+++ test_dir/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - function prints out a single character
+ *
+ * @c: character to be printed
+ *
+ * Return: On success 1
+ * On error, -1 is returned, errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
