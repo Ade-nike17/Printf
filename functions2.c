9b7fc28b3bcfea6925eb67732435154fca858f40
Ade-nike17 <perfectnyke@gmail.com>
2023-06-29 01:38:21 -0700

    split ptr_addr func and fix implementatn

--- functions2.c
+++ functions2.c
@@ -1,38 +1,48 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <stdarg.h>
-#include <stdint.h>
 #include "main.h"
 
 /**
- * print_ptr_addr - function prints a pointer address
+ * print_ptr_addr - prints a pointer address
  *
- * @args: variable lists of arguments
- *
- * Return: count
+ * @args: va_list containing the argument to print
+ * Return: number of characters printed
  */
-
 int print_ptr_addr(va_list args)
 {
 	void *ptr;
-	unsigned long address;
+	unsigned long int address;
 	int count = 0;
-	int i, digit;
-	char hex_digits[] = "0123456789abcdef";
 
 	ptr = va_arg(args, void *);
-	address = (unsigned long)ptr;
+	address = (unsigned long int)ptr;
 
-	_putchar('0');
-	_putchar('x');
+	count += _putchar('0');
+	count += _putchar('x');
+	count += print_hexadecimal_address(address);
 
-	/* Calculate the hexadecimal representation of the memory address */
-	for (i = sizeof(void *) * 2 - 1; i >= 0; i--)
-	{
-		digit = (address >> (i * 4)) & 0xf;
-		_putchar(hex_digits[digit]);
-		count++;
-	}
+	return (count);
+}
+
+
+/**
+ * print_hexadecimal_address - prints a hexadecimal address
+ *
+ * @address: the address to print
+ * Return: number of characters printed
+ */
+
+int print_hexadecimal_address(unsigned long int address)
+{
+	char hex_digits[] = "0123456789abcdef";
+	int count = 0;
+
+	if (address >= 16)
+	count += print_hexadecimal_address(address / 16);
+
+	count += _putchar(hex_digits[address % 16]);
+
 	return (count);
 }
